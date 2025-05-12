ac58811d85530341d85264ca74b343a371a8d3b5
Tristan Tarrant <ttarrant@redhat.com>
2025-05-12 18:22:06 +0200

    fixup! WIP

---
+++ commons/all/src/main/java/module-info.java
@@ -0,0 +1,22 @@
+module infinispan.commons {
+   requires com.github.benmanes.caffeine;
+   requires io.reactivex.rxjava3;
+   requires jakarta.transaction;
+   requires java.management;
+   requires java.naming;
+   requires java.security.sasl;
+   requires java.transaction.xa;
+   requires jcip.annotations;
+   requires jdk.management;
+   requires jdk.unsupported;
+   requires micrometer.core;
+   requires org.apache.logging.log4j;
+   requires org.apache.logging.log4j.core;
+   requires org.jboss.logging;
+   requires org.jboss.logging.annotations;
+   requires org.kohsuke.metainf_services;
+   requires org.reactivestreams;
+   requires protostream;
+   requires protostream.types;
+   requires reactor.blockhound;
+}
