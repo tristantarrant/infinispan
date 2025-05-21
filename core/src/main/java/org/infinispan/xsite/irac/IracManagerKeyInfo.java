f4b6b7f440eb5d9f9a42b80cc92961d6863ccc60
Pedro Ruivo <pruivo@redhat.com>
2025-05-21 20:46:45 +0100

    [#14865] NullPointerException while expiring entry with cross-site
    Signed-off-by: Pedro Ruivo <pruivo@redhat.com>

--- core/src/main/java/org/infinispan/xsite/irac/IracManagerKeyInfo.java
+++ core/src/main/java/org/infinispan/xsite/irac/IracManagerKeyInfo.java
@@ -3,10 +3,12 @@ package org.infinispan.xsite.irac;
 import static org.infinispan.commons.io.UnsignedNumeric.readUnsignedInt;
 import static org.infinispan.commons.io.UnsignedNumeric.writeUnsignedInt;
 
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
-import java.util.Objects;
+import org.infinispan.commons.marshall.ProtoStreamTypeIds;
+import org.infinispan.commons.util.Util;
+import org.infinispan.marshall.protostream.impl.MarshallableObject;
+import org.infinispan.protostream.annotations.ProtoFactory;
+import org.infinispan.protostream.annotations.ProtoField;
+import org.infinispan.protostream.annotations.ProtoTypeId;
 
 /**
  * @author Pedro Ruivo
@@ -39,13 +41,11 @@ public class IracManagerKeyInfo {
    @Override
    public boolean equals(Object o) {
       if (this == o) return true;
-      if (!(o instanceof IracManagerKeyInfo)) return false;
+      if (!(o instanceof IracManagerKeyInfo that)) return false;
 
-      IracManagerKeyInfo that = (IracManagerKeyInfo) o;
-
-      if (segment != that.getSegment()) return false;
-      if (!key.equals(that.getKey())) return false;
-      return owner.equals(that.getOwner());
+      return segment == that.getSegment() &&
+            key.equals(that.getKey()) &&
+            owner.equals(that.getOwner());
    }
 
    @Override
@@ -58,7 +58,7 @@ public class IracManagerKeyInfo {
 
    @Override
    public String toString() {
-      return "IracManagerKeyInfoImpl{" + "segment=" + segment + ", key=" + key + ", owner=" + owner + '}';
+      return "IracManagerKeyInfoImpl{" + "segment=" + segment + ", key=" + Util.toStr(key) + ", owner=" + owner + '}';
    }
 
    public static void writeTo(ObjectOutput output, IracManagerKeyInfo keyInfo) throws IOException {
